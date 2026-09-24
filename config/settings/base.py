b037cd422e6b8395e4466e61aff875b1ce0e10b8
Syed Arhan <arhannaqshbandi@gmail.com>
2026-09-24 09:18:06 +0530

    v5: install DRF, set global TokenAuthentication + IsAuthenticated defaults, add authtoken app

--- config/settings/base.py
+++ config/settings/base.py
@@ -25,6 +25,8 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "apps.accounts",
     "apps.tasks",
+    "rest_framework",
+    "rest_framework.authtoken",
 ]
 
 
@@ -97,3 +99,12 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "accounts.User"
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.TokenAuthentication",
+    ],
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
+    ],
+}
